ecbdc9b51399b534645473499b23345ce35d0861
Kawan Nakao <nakaokawan@gmail.com>
2022-04-11 15:26:04 -0300

    Adicionado o sistema de autentificação

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -7,19 +8,30 @@
   <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
   <title>@yield('title', 'Horas Complementares')</title>
 </head>
+
 <body>
   <!-- header -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
     <div class="container">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav ms-auto">
-          <a class="nav-link active" href="{{ route('horas_complementares.cadastrar') }}")>Cadastrar horas</a>
-          <a class="nav-link active" href="{{ route('horas_complementares.todasHoras') }}">Ver horas</a>
         </div>
+        @guest
+        <a class="nav-link active" href="{{ route('login') }}">Login</a>
+        <a class="nav-link active" href="{{ route('register') }}">Register</a>
+        @else
+        <div class="navbar-nav ms-auto">
+          <a class="nav-link active" href="{{ route('horas_complementares.cadastrar') }}" )>Cadastrar horas</a>
+          <a class="nav-link active" href="{{ route('horas_complementares.todasHoras') }}">Ver horas</a>
+        <form id="logout" action="{{ route('logout') }}" method="POST">
+          <a role="button" class="nav-link active" onclick="document.getElementById('logout').submit();">Logout</a>
+          @csrf
+        </form>
+        @endguest
+        <div class="vr bg-white mx-2 d-none d-lg-block"></div>
+      </div>
     </div>
   </nav>
 
@@ -37,4 +49,5 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
   </script>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-
+Auth::routes();
 Route::get('/', 'App\Http\Controllers\Home\HomeController@index')->name("home.index");;
 route::get('/horas_complementares/todas', 'App\Http\Controllers\Horas\HorasComplementaresController@todasHoras')->name("horas_complementares.todasHoras");
 Route::get('/horas_complementares', 'App\Http\Controllers\Horas\HorasComplementaresController@cadastrar')->name("horas_complementares.cadastrar");
