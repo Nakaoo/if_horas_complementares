ee95b28178c8607a45cfd6b04b5cf593f79818f9
Kawan <nakaokawan@gmail.com>
2022-05-02 16:54:32 -0300

    Ajustes de bugs

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
     public function index()
     {
         if(!Auth::user())
-            return view('home');
+            return view('home.index');
         else
             return redirect()->route('horas_complementares.dashboard');
     }
